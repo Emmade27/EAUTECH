7c9b14b823faf411557c986e8440ade72322d419
Emmade <emmanuelemyadegbenro@gmail.com>
2025-11-28 11:15:38 +0100

    EAUTECH Project

---
+++ public/signup/signup.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, GithubAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHHQF8VL1n9mpFccL8G8hZYf_GeNh7i54",
+    authDomain: "eautech-1a52c.firebaseapp.com",
+    databaseURL: "https://eautech-1a52c-default-rtdb.firebaseio.com",
+    projectId: "eautech-1a52c",
+    storageBucket: "eautech-1a52c.firebasestorage.app",
+    messagingSenderId: "666883701744",
+    appId: "1:666883701744:web:5be8a7cb6511f91c5fbb35"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const provider2 = new GithubAuthProvider();
+const database = getDatabase(app);
+
+
+const signIn = () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            console.log(user)
+            if (user) setTimeout(() => {
+                window.location.href = "../dashboard/dashboard.html"
+            }, 1000);
+            else {
+                window.location.href = "../public/index.html"
+            }
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            console.log(errorCode);
+        })
+}
+
+const github = () => {
+    signInWithPopup(auth, provider2)
+        .then((result) => {
+            const user = result.user;
+            console.log(user)
+            if (user) setTimeout(() => {
+                window.location.href = "../dashboard/dashboard.html"
+            }, 1000);
+            else {
+                window.location.href = "../public/index.html"
+            }
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            console.log(errorCode);
+        })
+}
+
+window.signIn = signIn
+window.github = github
